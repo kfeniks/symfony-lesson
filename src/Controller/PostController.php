0ecc0206ead3439b36b25a836f0ade260a479924
Dubinin A <keccgroup@gmail.com>
2019-11-21 19:30:18 +0200

    new action in controller Post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\PostType;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,6 +17,8 @@ class PostController extends AbstractController
 {
     /**
      * @Route("/", name="index")
+     * @param PostRepository $postRepository
+     * @return Response
      */
     public function index(PostRepository $postRepository)
     {
@@ -28,18 +31,58 @@ class PostController extends AbstractController
 
     /**
      * @Route("/create", name="create")
+     * @param Request $request
+     * @return Response
      */
     public function create(Request $request)
     {
         $post = new Post();
-        $post->setTitle('This is going');
+//        $post->setTitle('This is going');
+        $form = $this->createForm(PostType::class, $post);
 
-        // entity manager
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+            // entity manager
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+            $this->addFlash('success', 'Post was added');
+        }
+
+//        return new Response('Post was created');
+//        return $this->redirect($this->generateUrl('post.index'));
+        return $this->render('post/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/show/{id}", name="show")
+     * @param Post $post
+     * @return Response
+     */
+    public function show(Post $post)
+    {
+        //create the show view
+        return $this->render('post/show.html.twig', [
+            'post' => $post
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     * @param Post $post
+     * @return Response
+     */
+    public function remove(Post $post)
+    {
         $em = $this->getDoctrine()->getManager();
-        $em->persist($post);
+        $em->remove($post);
         $em->flush();
 
-        return new Response('Post was created');
+        $this->addFlash('success', 'Post was removed');
 
+        return $this->redirect($this->generateUrl('post.index'));
     }
 }
